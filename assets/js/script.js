7cc01c65f4435ea7e44bc39f775accb575bc81f3
Katie Buchholz <kzbindy@hotmail.com>
2024-05-07 21:10:08 -0400

    started working on the fetch requests and responses for the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 const APIkey = "3fa84feb1f3c2594bd2063cc9c508894"
+const error = "Please Enter Valid City"
 
-const weatherURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+preventDefault
 
+let city;
 
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+
+fetch(weatherURL)
+
+.then(function (response) {
+    return response.json();
+})
+
+.catch (function (error) {
+    return('Error $(error)')
+
+})
